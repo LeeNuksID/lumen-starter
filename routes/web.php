1b850591822445b79b4b6f9c011e7121dfddc318
Abdullah Ibnu Hibban <abd.ibnuhibban@gmail.com>
2021-01-23 10:44:09 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,14 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-
 $router->group(['prefix'=>'v1'], function() use($router){
 
 	// Authenticate
-	$router->post('/auth', 'AuthController@store');
+    $router->post('/register', 'AuthController@register');      // Register
+    $router->post('/auth', 'AuthController@store');             // Login
 	    
-    $router->post('auth/email/{id:[0-9]+}', 'AuthController@update_email');
-    $router->post('auth/password/{id:[0-9]+}', 'AuthController@update_pw');
-
-    $router->post('/register', 'AuthController@register');
+    $router->post('auth/email/{id:[0-9]+}', 'AuthController@update_email');     // Update Email
+    $router->post('auth/password/{id:[0-9]+}', 'AuthController@update_pw');     // Update Password
 	
 	$router->group(['middleware' => 'auth:api'], function() use ($router){
     	$router->get('/', function () use ($router){
